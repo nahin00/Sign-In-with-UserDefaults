a9e9acb4bedada363de5edf95b585e2bbd1685f2
Nahin Ahmed <nahin@siliconorchard.com>
2017-03-21 14:14:20 +0600

    Initial Commit

---
+++ Sign In with UserDefaults/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sign In with UserDefaults
+//
+//  Created by Silicon Orchard Ltd on 3/21/17.
+//  Copyright © 2017 Nahin Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
